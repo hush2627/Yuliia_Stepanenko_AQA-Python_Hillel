61167fe86963766a7147570fc33e77210b3f6219
yuliia_stepanenko <jsstepanenko@gmail.com>
2026-07-30 14:16:35 +0300

    Fixes for task_03 in homework 5.02

--- homework_05.02.py
+++ homework_05.02.py
@@ -7,48 +7,38 @@ from math import trunc
 from unittest import result
 
 people_records = [
-  ('John', 'Doe', 28, 'Engineer', 'New York'),
-  ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
-  ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
-  ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
-  ('Michael', 'Brown', 22, 'Student', 'Seattle'),
-  ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
-  ('David', 'Miller', 33, 'Software Developer', 'Austin'),
-  ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
-  ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
-  ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
-  ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
-  ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
-  ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
-  ('Ava', 'White', 42, 'Journalist', 'San Diego'),
-  ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
+    ('John', 'Doe', 28, 'Engineer', 'New York'),
+    ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
+    ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
+    ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
+    ('Michael', 'Brown', 22, 'Student', 'Seattle'),
+    ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
+    ('David', 'Miller', 33, 'Software Developer', 'Austin'),
+    ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
+    ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
+    ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
+    ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
+    ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
+    ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
+    ('Ava', 'White', 42, 'Journalist', 'San Diego'),
+    ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
 ]
 people_records.insert(0, ('Yuliia', 'Stepanenko', 28, 'QA', 'Kyiv'))
 print(people_records)
 
 for list_view in people_records:  ## хотіла вивести списком для зручності читання
-  print(list_view)
-before_swap=people_records[1], people_records[5]
+    print(list_view)
+before_swap = people_records[1], people_records[5]
 people_records[1], people_records[5] = people_records[5], people_records[1]
-after_swap=people_records[1], people_records[5]
+after_swap = people_records[1], people_records[5]
 print("before swap [1],[5]]) ", before_swap)
 print("after swap [1], [5]] ", after_swap)
 
+
+#FIXED VARIANT
 indexes = [6, 10, 13]
+all_people_equal_or_older_thirties = True
 for index in indexes:
-    print(people_records[index])
-    result = people_records[index][2] >=30
-    print(result)
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if people_records[index][2] < 30:
+        all_people_equal_or_older_thirties = False
+print("all people in the list have age >=30 : " ,all_people_equal_or_older_thirties)
